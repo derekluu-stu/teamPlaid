032d6baa82136b998f537d35768b522e98bdb2b8
studickie <dickie.stuart@gmail.com>
2019-02-20 17:09:32 -0500

    script set-up

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// project parent object
+const myTuneApp = {};
+
+// api call required info
+myTuneApp.apiUrl =  "http://ws.audioscrobbler.com/2.0/";
+myTuneApp.apiKey =  "ad9364740e28729d2afec7f390614ec2";
+
+myTuneApp.getData = function() {
+    
+}
+
+myTuneApp.init = function() {
+
+}
+
+$(function() {
+
+    myTuneApp.init();
+})
