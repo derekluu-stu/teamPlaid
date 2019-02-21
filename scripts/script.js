294a93e22502618ada051f0d02d5ef5bf4ad0e29
Derek <derekluu90@gmail.com>
2019-02-21 16:04:59 -0500

    working API request

--- scripts/script.js
+++ scripts/script.js
@@ -5,35 +5,66 @@ const myTuneApp = {};
 myTuneApp.apiUrl =  "http://ws.audioscrobbler.com/2.0/";
 myTuneApp.apiKey =  "ad9364740e28729d2afec7f390614ec2";
 
+//appends to the DOM
+myTuneApp.printResults = function (results1, results2){
+    console.log(results1, results2);
+    results2.forEach((track) => console.log(track.name))
+}
+
 
 // get user input
-myTuneApp.getUserInput = function (){
-    
-}
+myTuneApp.handleUserSearch = function (){
+    $('header form').on('submit', (e) => {
+        e.preventDefault();
+        const userInput = $('.searchBar>input[type="text"]').val();
+        this.getArtistData(userInput);
+    });
+};
 
 // searchQuery parameter: artist or genre name
 // searchType parameter: user selected artist or genre search style
-myTuneApp.getArtistData = function(){
-    $.ajax({
+myTuneApp.getArtistData = function(userArtistQuery){
+
+    ///artist info - includes artist name, images and 5 similar artist
+    const getArtistInfo = $.ajax({
         url: myTuneApp.apiUrl,
         data: {
             api_key: "ad9364740e28729d2afec7f390614ec2",
-            method: "artist.getsimilar",
-            artist: "pink floyd",
+            method: "artist.getinfo",
+            artist: userArtistQuery,
+            autocorrect: 1,
             format: "json"
         }
     })
-    .then((results) => {
-        console.log(results)
+
+    //top tracks of searched artist
+    const getArtistTracks = $.ajax({
+        url: myTuneApp.apiUrl,
+        data: {
+            api_key: "ad9364740e28729d2afec7f390614ec2",
+            method: "artist.gettoptracks",
+            artist: userArtistQuery,
+            limit: 25,
+            format: "json"
+        }
+    })
+
+    $.when(getArtistInfo, getArtistTracks)
+    .then((infoResults, trackResults) => {
+        // myTuneApp.printResults(getArtistInfo.artist, getArtistTracks.toptracks);
+        myTuneApp.printResults(getArtistInfo.responseJSON.artist, getArtistTracks.responseJSON.toptracks.track);
     })
     .fail((error) => {
         console.log(error)
     })
 };
 
+
 myTuneApp.init = function() {
 
-    this.getArtistData();
+    this.handleUserSearch();
+    // this.getArtistData();
+
 };
 
 $(function() {
