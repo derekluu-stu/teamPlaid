1ab5ac4e9b8b00ab639586a61e3386842403dbd2
studickie <dickie.stuart@gmail.com>
2019-02-21 17:53:39 -0500

    append to page in progress

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,22 @@ myTuneApp.apiUrl =  "http://ws.audioscrobbler.com/2.0/";
 myTuneApp.apiKey =  "ad9364740e28729d2afec7f390614ec2";
 
 //appends to the DOM
-myTuneApp.printResults = function (results1, results2){
-    console.log(results1, results2);
-    results2.forEach((track) => console.log(track.name))
+myTuneApp.printResults = function (results1, results2, searchType){
+    
+    const $songList = $(".songList>ol");
+    const $songImage = $(".songImage");
+    let artistName;
+
+    $songList.empty();
+    $songImage.empty();
+    // result1 image append
+    const image = results1["image"][results1["image"].length - 1]["#text"]
+   
+
+    // results2 will always be song names
+    results2.forEach((track) => {
+        $songList.append(`<li>${track.name}</li>`)
+    });
 }
 
 
@@ -16,7 +29,9 @@ myTuneApp.printResults = function (results1, results2){
 myTuneApp.handleUserSearch = function (){
     $('header form').on('submit', (e) => {
         e.preventDefault();
+
         const userInput = $('.searchBar>input[type="text"]').val();
+
         this.getArtistData(userInput);
     });
 };
@@ -29,7 +44,7 @@ myTuneApp.getArtistData = function(userArtistQuery){
     const getArtistInfo = $.ajax({
         url: myTuneApp.apiUrl,
         data: {
-            api_key: "ad9364740e28729d2afec7f390614ec2",
+            api_key: myTuneApp.apiKey,
             method: "artist.getinfo",
             artist: userArtistQuery,
             autocorrect: 1,
@@ -41,7 +56,7 @@ myTuneApp.getArtistData = function(userArtistQuery){
     const getArtistTracks = $.ajax({
         url: myTuneApp.apiUrl,
         data: {
-            api_key: "ad9364740e28729d2afec7f390614ec2",
+            api_key: myTuneApp.apiKey,
             method: "artist.gettoptracks",
             artist: userArtistQuery,
             limit: 25,
@@ -52,18 +67,37 @@ myTuneApp.getArtistData = function(userArtistQuery){
     $.when(getArtistInfo, getArtistTracks)
     .then((infoResults, trackResults) => {
         // myTuneApp.printResults(getArtistInfo.artist, getArtistTracks.toptracks);
-        myTuneApp.printResults(getArtistInfo.responseJSON.artist, getArtistTracks.responseJSON.toptracks.track);
+        myTuneApp.printResults(getArtistInfo.responseJSON.artist, getArtistTracks.responseJSON.toptracks.track, "searchQuery");
     })
     .fail((error) => {
         console.log(error)
     })
 };
 
+myTuneApp.getInitialData = function() {
+
+    const intialData = $.ajax({
+        url: myTuneApp.apiUrl,
+        data: {
+            api_key: myTuneApp.apiKey,
+            method: "geo.gettoptracks",
+            country: "Canada",
+            limit: 25,
+            format: "json"
+        }
+    })
+
+    .then((results) => {
+        myTuneApp.printResults(results["tracks"]["track"][0], 
+        results["tracks"]["track"], "initial");
+    })
+}
+
 
 myTuneApp.init = function() {
 
+    this.getInitialData();
     this.handleUserSearch();
-    // this.getArtistData();
 
 };
 
