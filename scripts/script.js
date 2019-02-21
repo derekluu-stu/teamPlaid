109f14a07666052c6cc8c6998c12d1dccef8a242
studickie <dickie.stuart@gmail.com>
2019-02-20 19:41:19 -0500

    working api request

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,30 @@ const myTuneApp = {};
 myTuneApp.apiUrl =  "http://ws.audioscrobbler.com/2.0/";
 myTuneApp.apiKey =  "ad9364740e28729d2afec7f390614ec2";
 
-myTuneApp.getData = function() {
-    
-}
+// searchQuery parameter: artist or genre name
+// searchType parameter: user selected artist or genre search style
+myTuneApp.getArtistData = function(){
+    $.ajax({
+        url: myTuneApp.apiUrl,
+        data: {
+            api_key: "ad9364740e28729d2afec7f390614ec2",
+            method: "artist.getsimilar",
+            artist: "pink floyd",
+            format: "json"
+        }
+    })
+    .then((results) => {
+        console.log(results)
+    })
+    .fail((error) => {
+        console.log(error)
+    })
+};
 
 myTuneApp.init = function() {
 
-}
+    this.getArtistData();
+};
 
 $(function() {
 
