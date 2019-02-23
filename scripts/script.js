052fa6110f612015ac7984d95f308418005b891c
studickie <dickie.stuart@gmail.com>
2019-02-23 12:45:35 -0500

    api return changes

--- scripts/script.js
+++ scripts/script.js
@@ -6,143 +6,95 @@ myTuneApp.apiUrl = "http://ws.audioscrobbler.com/2.0/";
 myTuneApp.apiKey = "ad9364740e28729d2afec7f390614ec2";
 
 //appends to the DOM
-myTuneApp.printResults = function (infoResults, trackResults, searchType){
-
-    // console.log(infoResults, trackResults, searchType);
+myTuneApp.printResults = function (artistInfoArray){
     
     const $songList = $(".songList>ol");
     const $songImage = $(".songImage");
-    // use searchType to determine where to find artist name data
-    const artistName = searchType === "multiSearch" ? infoResults["artist"]["name"] : infoResults["name"];
     
-    const image = infoResults["image"][infoResults["image"].length - 1]["#text"];
+    // nav'ed into artsit image array; used that to shorten the variable value of displayImage
+    const imageArray = artistInfoArray[0]["image"];
+    const displayImage = imageArray[imageArray.length - 1]["#text"];
+    const artistInfo = artistInfoArray[0]["artist"];
+    const artistName = artistInfo["name"];
     
-    if (searchType === 'multiSearch'){
-        const artistName = infoResults["artist"]["name"];
-        
-        trackResults.forEach((track) => {
-            $songList.append(`<li class="track" data-artist="${track.artistName}">${track.name}</li>`);
-        });
-        
-    } else if (searchType === 'singleSearch') {
-        const artistName = infoResults["name"]
-        
-        trackResults.forEach((track) => {
-            $songList.append(`<li class="track" data-artist="${track.artistName}">${track.name}</li>`);
-        });
-        
-        //copy the data into another array and we pull the artist we need in sync with the for loop
-    } else if (searchType === 'initial'){
-
-        console.log('this is our initial load');
-
-        const artistsArray = trackResults.map((tracks) => {
-            return tracks.artist.name;
-        });
-        console.log(artistsArray);
-
-        //artists: results["tracks"]["track"][0].artist.name
-
-        // const artistName = 
-
-        console.log(image)
-
-
-
-    }
-    
-    
-    
-    // empty display containers
-    $songList.empty();
-    $songImage.empty();
-    
-    // append image from infoResults    
-    $songImage.append(`<img src="${image}" alt="Image of ${artistName}">`);
-    
-    // append song names from trackResults
-    trackResults.forEach((track) => {
-        // const artistAttr = searchType === "multiSearch" ? infoResults["artist"]["name"] : infoResults["name"];
-        $songList.append(`<li class="track" data-artist="${track.artistName}">${track.name}</li>`);
-        // this works for click search
+    // append image   
+    $songImage.append(`<img src="${displayImage}" alt="image of ${artistName}" class="${artistName}" >`);
+    console.log(artistInfoArray)
+    //append track results
+    artistInfoArray.forEach((track) => {
+        $songList.append(`<li class="track" data-artist="${track["artist"]["name"]}">${track.name}</li>`);
     });
 }
-    
 
-    //genre song sample:
-    // results.tracks.track[0].name (drop name when passing)
-
-    //top artists by genre 
-    // const similarArtistsGenre = results3.topartists.artist
-
-
-// get user input
+// handle user text input & submit
 myTuneApp.handleUserSearch = function (){
 
     $('header form').on('submit', (event) => {
+        
         event.preventDefault();
 
         const $userInput = $('.searchBar>input[type="text"]');
         const $searchParameter = $('.searchFunction>input[name="searchParameter"]:checked');
 
         // search based on input value and search type selection
-        if($userInput.val().length > 0 && $searchParameter.attr("id") === "searchArtist"){
+        if($userInput.val() && $searchParameter.attr("id") === "searchArtist"){
             this.getArtistData($userInput.val());
 
-        } else if ($userInput.val().length > 0 && $searchParameter.attr("id") === "searchGenre"){
+        } else if ($userInput.val() && $searchParameter.attr("id") === "searchGenre"){
             this.getGenreData($userInput.val());
+
+        } else {
+            return null;
         }
 
         $userInput.val("")
     });
 };
 
+// handle user click-search 
 myTuneApp.handleClickSearch = function(){
 
     $(".songList").on("click", ".track", (event) => {
 
-        console.log("clicked");
-
         const $userClick = event.target;
+
         myTuneApp.getTrackData($userClick.textContent, $userClick.getAttribute("data-artist"));
     })
 };
 
-myTuneApp.getTrackData = function (userTrackQuery, userArtistQuery){
 
-    console.log("sending search query");
-    console.log(userTrackQuery, userArtistQuery);
+// ERROR --> ? hard coded for test
+// get tracks similar to user track slected by click 
+// required info -> artst name, id, image, top tracks
+myTuneApp.getTrackData = function (){
 
-    const getTrackInfo = $.ajax({
+    $.ajax({
         url: myTuneApp.apiUrl,
         data: {
             api_key: myTuneApp.apiKey,
             method: 'tracks.getsimilar',
-            track: userTrackQuery,
-            artist: userArtistQuery,
+            track: "the day",
+            artist: "the roots",
             limit: 25,
             format: 'json'
         }
     })
 
     .then((results) => {
-        myTuneApp.printResults(results["tracks"]["track"][0], results["tracks"]["track"], "searchQuery", "multiSearch");
-        // console.log(results);
+        // this.printResults(results["toptracks"]["track"]);
+        console.log(results)
     })
 
     .fail((error) => {
-        console.log("error");
+        console.log(error);
     })
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> stu_branch
-
-myTuneApp.getGenreData = function (userGenreQuery){
+// get searched genre info -> artst name, id, image, top tracks
+myTuneApp.getGenreData = function (genreQuery){
 
     //returns name of track, artist and image of artist
-    const getTrackInfo = $.ajax({
+    $.ajax({
         // URL and Method are always stated outside of the data object (in jquery docs)
         // No method used here, due to the nature of the API
         url: myTuneApp.apiUrl,
@@ -150,77 +102,14 @@ myTuneApp.getGenreData = function (userGenreQuery){
         data: {
             api_key: myTuneApp.apiKey,
             method: 'tag.getTopTracks',
-            tag: userGenreQuery,
+            tag: genreQuery,
             limit: 25,
             format: 'json'
         }
     })
 
-    //similar artists by genre 
-    // const getSimilarArtistGenre = $.ajax({
-    //     url: myTuneApp.apiUrl,
-    //     data: {
-    //         api_key: myTuneApp.apiKey,
-    //         method: "tag.getTopArtists",
-    //         tag: userGenreQuery ,
-    //         limit: 25,
-    //         format: 'json'
-    //     }
-    // })
-    // metadata: results3.topartists.artist 
     .then((results) => {
-        
-        myTuneApp.printResults(results["tracks"]["track"][0], results["tracks"]["track"], "searchQuery", "multiSearch")
-        // results.tracks.track[0].image[results.tracks.track[0].image.length - 1]["#text"];
-
-        //image is stored in an array which holds the track name and iimage
-            // track name is iterated
-            //image is not, how do we match the index of the track with the track name
-        //first figure out how the image will change based on what the user clicks
-
-        // myTuneApp.printResults(results.tracks.track[1], results.tracks.track);
-    })
-};
-
-
-
-
-// searchQuery parameter: artist or genre name
-// searchType parameter: user selected artist or genre search style
-myTuneApp.getArtistData = function(userArtistQuery){
-
-    ///artist info - includes artist name, images and 5 similar artist
-    const getArtistInfo = $.ajax({
-        url: myTuneApp.apiUrl,
-        data: {
-            api_key: myTuneApp.apiKey,
-            method: "artist.getinfo",
-            artist: userArtistQuery,
-            autocorrect: 1,
-            format: "json"
-        }
-    })
-
-    //top tracks of searched artist
-    const getArtistTracks = $.ajax({
-        url: myTuneApp.apiUrl,
-        data: {
-            api_key: myTuneApp.apiKey,
-            method: "artist.gettoptracks",
-            artist: userArtistQuery,
-            limit: 25,
-            format: "json"
-        }
-    })
-
-    $.when(getArtistInfo, getArtistTracks)
-
-    .then((infoResults, trackResults) => {
-        console.log(infoResults, trackResults)
-        // call printResults with parameters for infoResults, trackResults and searchType
-        myTuneApp.printResults(infoResults[0]["artist"], trackResults[0]["toptracks"]["track"], "singleSearch")
-        // console.log(infoResults[0]["artist"], trackResults[0]["toptracks"]["track"], "singleSearch")
-    // })
+        this.printResults(results["tracks"]["track"]);
     })
 
     .fail((error) => {
@@ -228,32 +117,58 @@ myTuneApp.getArtistData = function(userArtistQuery){
     })
 };
 
-myTuneApp.getInitialData = function() {
+// get searched artist info -> artst name, id, image, top tracks
+myTuneApp.getArtistData = function(artistQuery){
 
-    const intialData = $.ajax({
+ 
+    $.ajax({
         url: myTuneApp.apiUrl,
         data: {
             api_key: myTuneApp.apiKey,
-            method: "geo.gettoptracks",
-            country: "Canada",
+            method: "artist.gettoptracks",
+            artist: artistQuery,
             limit: 25,
             format: "json"
         }
     })
 
     .then((results) => {
-        console.log(results)
-        // call printResults with parameters for infoResults, trackResults and searchType
-        // console.log(results["tracks"]["track"], results["tracks"]["track"], "initial");
-        myTuneApp.printResults(results["tracks"]["track"], results["tracks"]["track"], "initial");
-        // artist and track (track is done)
+        this.printResults(results["toptracks"]["track"]);
+    })
+
+    .fail((error) => {
+        console.log(error)
+    })
+};
+
+// gets data of top tracks in user area on page load; currently hard-coded to Canada
+// required info -> artist name, id, image, top tracks
+myTuneApp.getTopTracksByLocation = function() {
+
+    $.ajax({
+        url: myTuneApp.apiUrl,
+        data: {
+            api_key: myTuneApp.apiKey,
+            method: "geo.gettoptracks",
+            country: "Canada", // make a variable in the future with geo API?
+            limit: 25,
+            format: "json"
+        }
+    })
+
+    .then((results) => {
+        this.printResults(results["tracks"]["track"])
+    })
+
+    .fail((error) => {
+        console.log(error)
     })
 };
 
 
 myTuneApp.init = function() {
 
-    this.getInitialData();
+    this.getTopTracksByLocation();
     this.handleUserSearch();
     this.handleClickSearch();
 };
